8d224db3c5fb726015bc889df45284d1dc5c64eb
Marco Schmidt <marco.schmidt@taugamma.eu>
2023-02-25 16:06:59 +0100

    Displays 'A' on QEMU output!

---
+++ modern/debug.gdb
@@ -0,0 +1,9 @@
+set width 0
+set height 0
+set verbose on
+
+target remote localhost:1234
+
+b kernel_main
+
+
---
+++ modern/debug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gdb --command=debug.gdb target/aarch64-unknown-none-softfloat/release/kernel 
+
+#kernel8.elf
+
